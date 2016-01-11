a8613317f6e9b47c061a357b04ff4d9b4d95d283
Brian Hillsley <bhills@brians-mbp.fios-router.home>
2016-01-10 20:12:38 -0500

    Basic structure for the Instagraham app project

---
+++ Instagraham/PhotoViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Instagraham
+//
+//  Created by Brian Hillsley on 1/10/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let clientID = "e05c462ebd86446ea48a5af73769b602"
+        
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientID)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            
+                    }
+                }
+        });
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
